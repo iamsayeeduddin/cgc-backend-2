cf3709ed7caf29d86414cf662d533467a618743a
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-09-11 08:01:50 +0530

    Express Routing

--- server-node.js
+++ server-node.js
@@ -17,3 +17,10 @@ server.listen(5000);
 // POST - Create Data - Sending The Data
 // PUT & PATCH - Updating the Data - Send & Update the Data
 // DELETE - Deleting the Data
+
+// HTTP RESPONSE STATUS CODE
+// 1xx - Informational
+// 2xx - Success - 200 - OK - 201 - Created - 204 - No Content
+// 3xx - Redirection
+// 4xx - Client Error - 400 - Bad Request - 401 - Unauthorized - 403 - Forbidden - 404 -
+// 5xx - Server Error - 500 - Internal Server Error - 502 - Bad Gatway - 503 - Service Unavailable
--- server.js
+++ server.js
@@ -4,4 +4,30 @@ const app = express();
 
 app.listen(5000, () => console.log("Server is Up & Running!"));
 
-// HTTP RESPONSE STATUS CODE
+app.post("/books", (req, res) => {
+  res.status(200);
+  res.json(books);
+});
+
+app.use("/", (req, res) => {
+  res.status(200);
+  res.send("Welcome to Express JS");
+});
+
+let books = [
+  {
+    id: 1,
+    name: "HTML & CSS",
+    price: 250,
+  },
+  {
+    id: 2,
+    name: "JavaScript",
+    price: 400,
+  },
+  {
+    id: 3,
+    name: "Backend with Node & Express JS",
+    price: 600,
+  },
+];
